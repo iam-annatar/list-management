2ac48c4707a888bdfde76be5bf047060144f162a
Annatar <itsannatar@gmail.com>
2025-11-01 11:00:52 +0330

    fix: conditionally render created lists and empty state

--- src/App.tsx
+++ src/App.tsx
@@ -35,41 +35,41 @@ function App() {
         <ListsEmptyState
           onOpenCreateModal={() => setIsManageListModalOpen(true)}
         />
-      ) : null}
-
-      <section
-        aria-label="lists management"
-        className="w-full px-5 pt-5 md:pt-20"
-      >
-        <div className="mb-8">
-          <TriggerCreateListModal onOpenCreateModal={openCreateModal} />
-        </div>
-        <div
-          aria-label="Added Lists Page title"
-          className="flex w-full items-center gap-7"
+      ) : (
+        <section
+          aria-label="lists management"
+          className="w-full px-5 pt-5 md:pt-20"
         >
-          <p className="text-size-xl leading-8 font-bold text-nowrap">
-            Created Lists
-          </p>
-          <div className="bg-primary-border h-px w-full" />
-        </div>
-        <div className="flex w-full flex-col gap-7">
-          <p className="text-size-lg text-secondary leading-7">
-            Here you can manage your lists easily.
-          </p>
-          <ul className="grid grid-cols-1 gap-4 pb-8 sm:grid-cols-2 lg:grid-cols-3">
-            {lists?.map((list) => (
-              <li key={list.id}>
-                <ListItemCard
-                  onDelete={deleteListItem}
-                  onOpenEditModal={() => openEditModal(list)}
-                  {...list}
-                />
-              </li>
-            ))}
-          </ul>
-        </div>
-      </section>
+          <div className="mb-8">
+            <TriggerCreateListModal onOpenCreateModal={openCreateModal} />
+          </div>
+          <div
+            aria-label="Added Lists Page title"
+            className="flex w-full items-center gap-7"
+          >
+            <p className="text-size-xl leading-8 font-bold text-nowrap">
+              Created Lists
+            </p>
+            <div className="bg-primary-border h-px w-full" />
+          </div>
+          <div className="flex w-full flex-col gap-7">
+            <p className="text-size-lg text-secondary leading-7">
+              Here you can manage your lists easily.
+            </p>
+            <ul className="grid grid-cols-1 gap-4 pb-8 sm:grid-cols-2 lg:grid-cols-3">
+              {lists?.map((list) => (
+                <li key={list.id}>
+                  <ListItemCard
+                    onDelete={deleteListItem}
+                    onOpenEditModal={() => openEditModal(list)}
+                    {...list}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      )}
 
       <ManageListModal
         initialValues={selectedList}
